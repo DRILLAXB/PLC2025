f04ce7b936ea0ee89cc7ddc7a2d0b353e6de5bf4
DRILLAXB Balatharan <dilax511@hotmail.com>
2025-01-29 09:40:25 +0000

    allows a more flexible user input for inpFunc and if statements changed to guards

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,20 @@
 --create inpFunc
-inpFunc = [1..5] 
+getValues = do
+    putStrLn "Enter start number (c):"
+    cStr <- getLine
+    putStrLn "Enter the end number (b):"
+    bStr <- getLine
+    let c = read cStr :: Int
+    let b = read bStr :: Int
+    return [c..b] 
 
 --Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
-
+-- applicatorFunc inpFunc s = if s=='s' then fromIntegral (sum inpFunc) else  fromIntegral (sum inpFunc)/5  
+applicatorFunc inpFunc s 
+    |  s=='s' = fromIntegral (sum inpFunc) 
+    | otherwise =   fromIntegral (sum inpFunc)/5  
+    
 main = do
+    inpFunc <- getValues
     let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
     putStrLn("sum = " ++ show(result))
