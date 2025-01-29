b9b064d6f21ec481b055624175beb32718aecea3
DRILLAXB <dilax511@hotmail.com>
2025-01-29 09:01:25 +0000

    Update task1.hs
    changed variable names

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -4,8 +4,12 @@ main =
     do
     print (myFunc1 5)
     --print (MyFunc2 10)
+    print(myFunc2 10)
     --print (3rdFunc)
+    print(thirdFunc)
 
 myFunc1 x = x*10
 --MyFunc2 x = x*2
 --3rdFunc = "Hello, this is 3rd Func"
+myFunc2 x = x*2
+thirdFunc = "Hello, this is 3rd Func"
