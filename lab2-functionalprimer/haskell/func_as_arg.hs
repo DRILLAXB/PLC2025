97c7b697b96642c90eb56a0ad1c36a9019a5ed43
DRILLAXB <dilax511@hotmail.com>
2025-01-30 19:11:37 +0000

    Update func_as_arg.hs
    correcting mistakes on the lab

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,20 +1,32 @@
---create inpFunc
-getValues = do
-    putStrLn "Enter start number (c):"
-    cStr <- getLine
-    putStrLn "Enter the end number (b):"
-    bStr <- getLine
-    let c = read cStr :: Int
-    let b = read bStr :: Int
-    return [c..b] 
+-- --create inpFunc
+-- getValues = do
+--     putStrLn "Enter start number (c):"
+--     cStr <- getLine
+--     putStrLn "Enter the end number (b):"
+--     bStr <- getLine
+--     let c = read cStr :: Int
+--     let b = read bStr :: Int
+--     return [c..b] 
+
+-- --Define applicatorFunc
+-- -- applicatorFunc inpFunc s = if s=='s' then fromIntegral (sum inpFunc) else  fromIntegral (sum inpFunc)/5  
+-- applicatorFunc inpFunc s 
+--     |  s=='s' = fromIntegral (sum inpFunc) 
+--     | otherwise =   fromIntegral (sum inpFunc)/5  
+    
+-- main = do
+--     inpFunc <- getValues
+--     let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
+--     putStrLn("sum = " ++ show(result))
+
+
+-- create inpFun
+inpFunc a b = [a..b]
 
 --Define applicatorFunc
--- applicatorFunc inpFunc s = if s=='s' then fromIntegral (sum inpFunc) else  fromIntegral (sum inpFunc)/5  
-applicatorFunc inpFunc s 
-    |  s=='s' = fromIntegral (sum inpFunc) 
-    | otherwise =   fromIntegral (sum inpFunc)/5  
-    
+applicatorFunc inpFunc a b s = if s == 's' then sum (inpFunc a b) else sum (inpFunc a b))/(b-a+1)
+
+
 main = do
-    inpFunc <- getValues
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+    let result = applicatorFunc inpFun 1 10 's'
+    putStrLn("sum = " ++ show result)
