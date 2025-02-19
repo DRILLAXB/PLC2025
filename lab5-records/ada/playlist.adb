19c4da9ae321cf74dbb19ced90baa109d5ccf4d2
DRILLAXB Balatharan <dilax511@hotmail.com>
2025-02-19 18:07:57 +0000

    facing compilation issues for ada tasks

--- lab5-records/ada/playlist.adb
+++ lab5-records/ada/playlist.adb
@@ -14,21 +14,43 @@ procedure PlayList is
         Put (To_String (p.name));
     end Put_Person;
 
-    type Item is record
-        name        : Unbounded_String;
-        performer   : Person;
-        length_secs : Float;
+    type ItemVariant is (pause, piece);
+
+    type Item(variant : ItemVariant) is record
+        
+      
+      case variant is 
+         when piece =>
+            name        : Unbounded_String;
+            performer   : Person;
+            length_secs : Float;
+         when pause =>
+            pause_length_secs : Float;
+            
+         
+
+            
+      end case;
+
     end record;
 
     procedure Put_Item (i : Item) is
-    begin
-        Put (To_String (i.name));
-        Put (" by ");
-        Put_Person (i.performer);
-        Put (" (");
-        Put (i.length_secs, aft => 1, exp => 0);
-        Put ("s)");
-    end Put_Item;
+      begin
+         case i.variant is
+            when piece =>
+                  Put(To_String(i.name));                -- Print the name of the piece
+                  Put(" by ");                            -- Print separator
+                  Put_Person(i.performer);                -- Print performer
+                  Put(" (");                              -- Print opening parenthesis
+                  Put(i.length_secs, aft => 1, exp => 0); -- Print length in seconds
+                  Put(" s)");                             -- Print closing parenthesis for piece
+            when pause =>
+                  Put("Pause: Length = ");                -- Print pause label
+                  Put(i.pause_length_secs, aft => 1, exp => 0); -- Print length of the pause
+                  Put(" s");                              -- Print seconds for pause
+         end case;
+      end Put_Item;
+    
 
     piece1 : Item :=
        (name => To_Unbounded_String ("Moonlight Sonata"),
@@ -36,15 +58,15 @@ procedure PlayList is
         length_secs => 17.0*60.0+26.0
        );
 
-    -- pause1 : Item :=
-    --    (
-    --     item_variant => PAUSE,
-    --     length_secs => 5.0
-    --    );
+    pause1 : Item :=
+      (
+       item_variant => PAUSE,
+       length_secs => 5.0
+      );
 
 begin
     Put_Item (piece1);
     Put_Line ("");
-    -- Put_Item(pause1);
-    -- Put_Line ("");
+    Put_Item(pause1);
+    Put_Line ("");
 end PlayList;
